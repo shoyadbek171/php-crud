94f25e15fc3e39ff9ca7b7099fbc53b5872f928f
Shoyadbek Odilov <shoyadbekodilov2@gmail.com>
2025-09-20 12:05:18 +0500

    same changed

--- database.php
+++ database.php
@@ -1,1 +1,17 @@
 <?php
+
+$conn = mysqli_connect("localhost", "shoyadbek", "shoyadbek123!", "php_crud");
+
+if ($conn) {
+
+    // INSERT so‘rovi to‘g‘ri sintaksisda
+    $conn->query("INSERT INTO users (id,name, mobile_number, created_at) VALUES (2,'shoyadbek', '901487981', NOW())");
+
+    // SELECT so‘rovi
+    $users = $conn->query("SELECT * FROM users")->fetch_all();
+
+    var_dump($users);
+
+} else {  
+    echo "Ulanishda xato";
+}
